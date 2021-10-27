a2279f11c1e677c285be92ab2cdc27f5b6957401
yusshu <yushu@unnamed.team>
2021-10-27 09:06:03 -0500

    feat: setup maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,34 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+val snapshotRepository: String by project
+val releaseRepository: String by project
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    publishing {
+        repositories {
+            maven {
+                url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                    uri(snapshotRepository)
+                } else {
+                    uri(releaseRepository)
+                }
+                credentials {
+                    username = project.properties["UNNAMED_REPO_USER"] as String?
+                        ?: System.getenv("REPO_USER")
+                    password = project.properties["UNNAMED_REPO_PASSWORD"] as String?
+                        ?: System.getenv("REPO_PASSWORD")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                from(getComponents().getByName("java"))
+            }
+        }
+    }
 }
