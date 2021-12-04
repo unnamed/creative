60f159146d2a70f37e7a44e53fb62a6450206ad5
yusshu <yushu@unnamed.team>
2021-12-03 23:52:12 -0500

    chore: add getSender getter to Uracle to get the ResourcePackSender implementation

--- src/main/java/team/unnamed/uracle/Uracle.java
+++ src/main/java/team/unnamed/uracle/Uracle.java
@@ -1,5 +1,7 @@
 package team.unnamed.uracle;
 
+import team.unnamed.uracle.resourcepack.ResourcePackSender;
+
 /**
  * Represents the entry point object for the Uracle
  * API
@@ -8,4 +10,14 @@ package team.unnamed.uracle;
  * can read and update state and call actions</p>
  */
 public interface Uracle {
+
+    /**
+     * Returns the {@link ResourcePackSender} that this
+     * service can provide, it is responsible for sending
+     * resource packs to players
+     *
+     * @return Returns the resource pack sender
+     */
+    ResourcePackSender getSender();
+
 }
