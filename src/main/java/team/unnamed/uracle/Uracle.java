ecb7c849cbf58bf3353f23ad8f1326fdc9d0b983
yusshu <yushu@unnamed.team>
2021-12-03 23:39:37 -0500

    chore: add 'Uracle' interface as API entry point

---
+++ src/main/java/team/unnamed/uracle/Uracle.java
@@ -0,0 +1,11 @@
+package team.unnamed.uracle;
+
+/**
+ * Represents the entry point object for the Uracle
+ * API
+ *
+ * <p>Everything is accessed via this interface, plugins
+ * can read and update state and call actions</p>
+ */
+public interface Uracle {
+}
