dbcb18f3e92cb1243526e8cc3528d3e5f963921e
yusshu <yushu@unnamed.team>
2021-12-12 13:09:17 -0500

    chore: add Sound writable data

--- api/src/main/java/team/unnamed/uracle/sound/Sound.java
+++ api/src/main/java/team/unnamed/uracle/sound/Sound.java
@@ -1,10 +1,18 @@
 package team.unnamed.uracle.sound;
 
 import team.unnamed.uracle.ResourceLocation;
+import team.unnamed.uracle.base.Writable;
 
 public class Sound {
 
     private ResourceLocation location;
+
+    /**
+     * The OGG sound data, located at the specified {@code location}
+     * inside assets/&lt;namespace&gt;/sounds folder
+     */
+    private Writable data;
+
     private float volume;
     private float pitch;
     private int weight;
