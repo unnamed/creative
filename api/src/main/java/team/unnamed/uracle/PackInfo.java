ce08578011fe8443aff138c60b9b2dcfd5cea623
yusshu <yushu@unnamed.team>
2021-12-13 10:09:23 -0500

    chore: add getters and serialization to PackInfo class

--- api/src/main/java/team/unnamed/uracle/PackInfo.java
+++ api/src/main/java/team/unnamed/uracle/PackInfo.java
@@ -1,6 +1,8 @@
 package team.unnamed.uracle;
 
-public class PackInfo {
+import java.util.Objects;
+
+public class PackInfo implements Element.Part {
 
     private final int format;
     private final String description;
@@ -8,6 +10,41 @@ public class PackInfo {
     public PackInfo(int format, String description) {
         this.format = format;
         this.description = description;
+    }
+
+    public int getFormat() {
+        return format;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void write(TreeWriter.Context context) {
+        context.writeIntField("format", format);
+        context.writeStringField("description", description);
+    }
+
+    @Override
+    public String toString() {
+        return "PackInfo{" +
+                "format=" + format +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PackInfo packInfo = (PackInfo) o;
+        return format == packInfo.format && description.equals(packInfo.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(format, description);
     }
 
 }
