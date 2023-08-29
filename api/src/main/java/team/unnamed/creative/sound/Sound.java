1466bbfbc00fa32da66b05e1d1d81406b8853d10
yusshu <yushuverduras1309@gmail.com>
2023-08-29 16:57:24 -0500

    docs: add javadoc to Sound class

--- api/src/main/java/team/unnamed/creative/sound/Sound.java
+++ api/src/main/java/team/unnamed/creative/sound/Sound.java
@@ -27,13 +27,22 @@ import net.kyori.adventure.key.Key;
 import net.kyori.adventure.key.Keyed;
 import net.kyori.examination.Examinable;
 import net.kyori.examination.ExaminableProperty;
+import net.kyori.examination.string.StringExaminer;
 import org.jetbrains.annotations.NotNull;
 import team.unnamed.creative.base.Writable;
+import team.unnamed.creative.texture.Texture;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Represents a Minecraft sound (.OGG) in the resource-pack,
+ * analog to {@link Texture}.
+ *
+ * @since 1.0.0
+ */
 public final class Sound implements Keyed, Examinable {
 
     private final Key key;
@@ -44,13 +53,51 @@ public final class Sound implements Keyed, Examinable {
         this.data = requireNonNull(data, "data");
     }
 
+    /**
+     * Returns this sound's key (location), the key contains
+     * the sound namespace and path, so that the full sound
+     * path is formatted like {@code assets/<namespace>/sounds/<path>}
+     *
+     * <p>Example key for a sound: <pre>{@code
+     *
+     *   Key key = Key.key("minecraft", "ambient/cave");
+     * }</pre>
+     * Note that the key value must not include the sound extension</p>
+     *
+     * @return The sound key
+     */
     @Override
     public @NotNull Key key() {
         return key;
     }
 
+    /**
+     * Returns the sound's OGG sound data, as a
+     * {@link Writable} object, never null
+     *
+     * @return The OGG sound data
+     * @since 1.0.0
+     */
     public Writable data() {
         return data;
+    }
+
+    @Override
+    public String toString() {
+        return examine(StringExaminer.simpleEscaping());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sound sound = (Sound) o;
+        return key.equals(sound.key) && data.equals(sound.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, data);
     }
 
     @Override
