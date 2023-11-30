b14f779060fac4056a944eeae25298af80e348a9
yusshu <yushu@unnamed.team>
2023-11-30 17:44:23 -0500

    deps(seriallizer-minecraft): update gson to 2.10.1

--- serializer-minecraft/build.gradle.kts
+++ serializer-minecraft/build.gradle.kts
@@ -6,7 +6,7 @@ description = "Minecraft: Java Edition vanilla serialization for the creative AP
 
 dependencies {
     api(project(":creative-api"))
-    api("com.google.code.gson:gson:2.8.0")
+    api("com.google.code.gson:gson:2.10.1")
     implementation("net.kyori:adventure-text-serializer-legacy:4.14.0")
     implementation("net.kyori:adventure-text-serializer-gson:4.14.0")
 }
